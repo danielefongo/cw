bd6aa656255122d4366b0a84f9266a14eebfda43
danielefongo <danielefongo@gmail.com>
2018-10-21 14:35:00 +0200

    convert provisioning.sh to Dockerfile commands during build

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #/bin/bash
 
+cat Dockerfile_base > Dockerfile
+echo -e "\n\n#PROVISIONING" >> Dockerfile
+
+cat provisioning.sh | while read line
+do
+	echo $line
+	if [[ "$line" ]]; then
+		echo "RUN $line" >> Dockerfile
+	fi
+done
+
 docker build . -t command_wrapper
