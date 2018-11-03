1451705908ccdb499d27eb81f9cf4ba8e9dddfa0
danielefongo <danielefongo@gmail.com>
2018-11-03 09:52:00 +0100

    add network between containers

--- wrapper.sh
+++ wrapper.sh
@@ -1,24 +1,27 @@
 source cw.properties
 
-__random_container_id() {
-	echo "cw_"$((1 + RANDOM % 1000))
-}
+# -------- Main functions -------- 
 
 function cw() {
 	command="eval \"$@\""
 	command=`echo "$command" | sed 's/cw //'`;
 	id=`__random_container_id`
+	__create_docker_net
 	docker run --rm --name $id \
 		-v `pwd`:$MOUNT_DIR -v $EXTERNAL_USER_HOME:/root \
+		--network $IMAGE_NAME \
 		-it $IMAGE_NAME \
-		bash -c "$command"
+		sh -c "$command"
+	__delete_docker_net
 }
 
 function cwd() {
 	name=$1
 	if [[ "$name" ]] && [[ -z `docker ps | grep $name` ]]; then
+		__create_docker_net
 		docker run -d --name $name \
 		-v `pwd`:$MOUNT_DIR -v $EXTERNAL_USER_HOME:/root \
+		--network $IMAGE_NAME \
 		-i $IMAGE_NAME 1>/dev/null
 		echo "Container $name created. Use \"cwa $name\" to attach a shell."
 	fi
@@ -29,6 +32,7 @@ function cwdd() {
 	if [[ "$name" ]] && [[ `docker ps | grep $name` ]]; then
 		docker rm -f $name 1>/dev/null
 		echo "Container $name deleted."
+		__delete_docker_net
 	fi
 }
 
@@ -40,6 +44,24 @@ function cwa() {
 	name=$1
 	if [[ "$name" ]] && [[ `docker ps | grep $name` ]]; then
 		docker exec -it "$name" sh
+	fi
+}
+
+# -------- Util functions -------- 
+
+function __random_container_id() {
+	echo "cw_"$((1 + RANDOM % 1000))
+}
+
+function __create_docker_net() {
+	if ! [[ `docker network ls | grep -e "[a-z0-9]*\s$IMAGE_NAME\s"` ]]; then
+		docker network create $IMAGE_NAME 1>/dev/null
+	fi
+}
+
+function __delete_docker_net() {
+	if [[ -z "`cwl`" ]]; then
+		docker network rm $IMAGE_NAME 1>/dev/null
 	fi
 }
 
